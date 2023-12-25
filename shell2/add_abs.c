7ac350957a63f95d69ede78dee34b7bc50d5f661
shakiru137 <yusufshakiruoluwasegun@gmail.com>
2023-12-25 13:30:10 +0100

    updated

---
+++ shell2/add_abs.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+
+char *add_abs_path_to_cmd(char *cmd)
+{
+        char *path = getenv("PATH");
+        char *path_copy;
+        char *full_path;                                                char *dir;
+        char *token;                                            
+        if (strchr(cmd, '/') == NULL)
+        {
+                path_copy = strndup(path, strlen(path));
+
+                token = _strtok(path_copy, ":");                                while (token != NULL)
+                {
+                        full_path = malloc(strlen(token) + strlen(cmd) + 2);                                                                            strcpy(full_path, token);                                       strcat(full_path, "/");                                         strcat(full_path, cmd);                                                                                                         if (access(full_path, X_OK) == 0)
+                        {
+                                free(path_copy);
+                                return full_path;
+                        }
+                                                                                        free(full_path);
+                        token = _strtok(NULL, ":");                             }                                               
+                my_print(cmd);                                                  my_print(": Command not found\n");
+                free(path_copy);                                                exit(1);                                                }
+        else
+        {
+                return cmd;
+        }                                                       }
