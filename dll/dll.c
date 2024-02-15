9d4ab062b2119413dd7b9896ae2b60cd77250740
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-02-15 21:57:27 +0100

    updated

---
+++ dll/dll.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+	int data;
+	struct Node* next;
+	struct Node* prev;
+};
+struct Node* head; // Global variable - pointer to head node
+struct Node* GetNewNode(int x)
+{
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+	newNode->data = x;
+	newNode->prev = NULL;
+	newNode->next = NULL;
+
+	return (newNode);
+}
+void InsertAtHead(int x)
+{
+	struct Node* newNode = GetNewNode(x);
+	if(head == NULL)
+	{
+		head = newNode;
+		return;
+	}
+	head->prev = newNode;
+	newNode = head;
+	head = newNode;
+}
+void InsertAtTail(int x)
+{
+	struct Node* temp = head;
+	struct Node* newNode = GetNewNode(x);
+
+	if (head == NULL)
+	{
+		head = newNode;
+		return;
+	}
+	while (temp->next != NULL) temp = temp->next;
+	temp->next = newNode;
+	newNode->prev = temp;
+}
+void print()
+{
+	struct Node* temp = head;
+	printf("Forward: ");
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+void ReversePrint()
+{
+	struct Node* temp = head;
+	if (temp == NULL) return;
+
+	while (temp->next != NULL)
+	temp = temp->next;
+
+	printf("Reverse: ");
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->prev;
+	}
+	printf("\n");
+}
+int main()
+{
+	head = NULL;
+	InsertAtHead(2); print(); ReversePrint();
+	InsertAtHead(4); print(); ReversePrint();
+	InsertAtHead(6); print(); ReversePrint();
+	InsertAtHead(8); print(); ReversePrint();
+
+	return (0);
+}
---
+++ hashTable/README.md
@@ -0,0 +1,5 @@
+Hash Table
+
+A hash table is an array with a hash function
+it is going to take some input and map that input to a location in our array
+hash("name") -> index
